14d2620c24b3e342f663bf19edfc8d38badceb6f
Brandon <kongbrandon0@gmail.com>
2025-03-01 19:42:26 -0600

    NVIM CONFIGS

---
+++ init.lua
@@ -0,0 +1,19 @@
+----- Require -----
+require("config.lazy")
+
+----- Basic configurations -----
+
+-- Opt configs
+vim.opt.number = true
+vim.opt.termguicolors = true
+
+-- Global configs
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+
+----- Theme configurations -----
+vim.cmd[[colorscheme tokyonight]]
+
+----- Setup -----
+
