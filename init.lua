8074b9e5d73c51b7bfbf85a041bfa56769b82a8c
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2025-03-03 12:46:53 -0600

    plugin(wakatime): added wakatime

--- init.lua
+++ init.lua
@@ -9,6 +9,8 @@ vim.opt.termguicolors = true
 vim.opt.undofile = false
 vim.opt.termguicolors = true
 
+vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
