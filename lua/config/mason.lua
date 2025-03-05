5735b38ec89b626fd8325ddd2f3f93aa3e3adc46
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2025-03-04 22:15:27 -0600

    feat: added cpp and java

--- lua/config/mason.lua
+++ lua/config/mason.lua
@@ -1,10 +1,13 @@
 require("mason").setup({})
 require("mason-lspconfig").setup({
-    ensure_installed = {'lua_ls', 'ts_ls', 'pyright' },
+    ensure_installed = {'clangd', 'lua_ls', 'ts_ls', 'clangd', 'java_language_server' },
     automatic_installation = true
 })
 
 require("lspconfig").lua_ls.setup {}
 require("lspconfig").pyright.setup {}
 require("lspconfig").ts_ls.setup {}
+require("lspconfig").clangd.setup {}
+require("lspconfig").java_language_server.setup {}
+
 
