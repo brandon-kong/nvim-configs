e3d8589ce2a4d08459284ae8d21748a2e70d8e33
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2025-03-04 20:22:18 -0600

    feat: set up python, ts, lua language servers

--- lua/config/mason.lua
+++ lua/config/mason.lua
@@ -1,5 +1,10 @@
 require("mason").setup({})
 require("mason-lspconfig").setup({
-    ensure_installed = {'lua_ls', 'ts_ls', 'pyright' }
-
+    ensure_installed = {'lua_ls', 'ts_ls', 'pyright' },
+    automatic_installation = true
 })
+
+require("lspconfig").lua_ls.setup {}
+require("lspconfig").pyright.setup {}
+require("lspconfig").ts_ls.setup {}
+
