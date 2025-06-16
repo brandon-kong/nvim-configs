7560471523c859adc104db96d1dd21c22ad15db6
Brandon Kong <95515854+brandon-kong@users.noreply.github.com>
2025-06-15 21:53:29 -0500

    refactor: organized configs and plugins

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -9,4 +9,12 @@ vim.keymap.set('n', '<leader>ps', function ()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 
+-- Clipboard remaps
 
+-- Yank to system clipboard
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+
+-- Paste from system clipboard
+vim.keymap.set("n", "<leader>p", '"+p')
+vim.keymap.set("v", "<leader>p", '"+p')
